d4438301c360267143b3be2a27ea88923067a09f
Mardon <markusmenzel@gmail.com>
2013-08-10 17:02:27 +0200

    Added GalaxyICS governor and fix compile error

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -397,6 +397,9 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GALAXYICS)
+extern struct cpufreq_governor cpufreq_gov_galaxyics;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_galaxyics)
 #endif
 
 
